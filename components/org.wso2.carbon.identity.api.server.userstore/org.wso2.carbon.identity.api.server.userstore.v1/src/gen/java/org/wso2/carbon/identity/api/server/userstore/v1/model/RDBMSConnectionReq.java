d34d3e561022522c430b5d0c4c889de04fd2263e
kanapriya <priyakules9@gmail.com>
2019-11-20 15:11:31 +0530

    Update yaml file and generate stub

--- components/org.wso2.carbon.identity.api.server.userstore/org.wso2.carbon.identity.api.server.userstore.v1/src/gen/java/org/wso2/carbon/identity/api/server/userstore/v1/model/RDBMSConnectionReq.java
+++ components/org.wso2.carbon.identity.api.server.userstore/org.wso2.carbon.identity.api.server.userstore.v1/src/gen/java/org/wso2/carbon/identity/api/server/userstore/v1/model/RDBMSConnectionReq.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.wso2.carbon.identity.api.server.userstore.v1.model;
 
@@ -32,21 +32,21 @@ import javax.validation.Valid;
 import javax.xml.bind.annotation.*;
 @ApiModel(description = "RDBMS Connection Request.")
 public class RDBMSConnectionReq  {
-  
+
     private String driverName;
     private String connectionURL;
     private String username;
-    private String password;
+    private String connectionPassword;
 
     /**
-    * Driver Name.
-    **/
+     * Driver Name.
+     **/
     public RDBMSConnectionReq driverName(String driverName) {
 
         this.driverName = driverName;
         return this;
     }
-    
+
     @ApiModelProperty(example = "com.mysql.jdbc.Driver", required = true, value = "Driver Name.")
     @JsonProperty("driverName")
     @Valid
@@ -60,14 +60,14 @@ public class RDBMSConnectionReq  {
     }
 
     /**
-    * The Connection URL.
-    **/
+     * The Connection URL.
+     **/
     public RDBMSConnectionReq connectionURL(String connectionURL) {
 
         this.connectionURL = connectionURL;
         return this;
     }
-    
+
     @ApiModelProperty(example = "jdbc:mysql://192.168.48.154:3306/test", required = true, value = "The Connection URL.")
     @JsonProperty("connectionURL")
     @Valid
@@ -81,14 +81,14 @@ public class RDBMSConnectionReq  {
     }
 
     /**
-    * The username.
-    **/
+     * The username.
+     **/
     public RDBMSConnectionReq username(String username) {
 
         this.username = username;
         return this;
     }
-    
+
     @ApiModelProperty(example = "root", required = true, value = "The username.")
     @JsonProperty("username")
     @Valid
@@ -102,22 +102,24 @@ public class RDBMSConnectionReq  {
     }
 
     /**
-    * The password.
-    **/
-    public RDBMSConnectionReq password(String password) {
+     * The password.
+     **/
+    public RDBMSConnectionReq connectionPassword(String connectionPassword) {
 
-        this.password = password;
+        this.connectionPassword = connectionPassword;
         return this;
     }
-    
-    @ApiModelProperty(example = "root", value = "The password.")
-    @JsonProperty("password")
+
+    @ApiModelProperty(example = "root", required = true, value = "The password.")
+    @JsonProperty("connectionPassword")
     @Valid
-    public String getPassword() {
-        return password;
+    @NotNull(message = "Property connectionPassword cannot be null.")
+
+    public String getConnectionPassword() {
+        return connectionPassword;
     }
-    public void setPassword(String password) {
-        this.password = password;
+    public void setConnectionPassword(String connectionPassword) {
+        this.connectionPassword = connectionPassword;
     }
 
 
@@ -133,14 +135,14 @@ public class RDBMSConnectionReq  {
         }
         RDBMSConnectionReq rdBMSConnectionReq = (RDBMSConnectionReq) o;
         return Objects.equals(this.driverName, rdBMSConnectionReq.driverName) &&
-            Objects.equals(this.connectionURL, rdBMSConnectionReq.connectionURL) &&
-            Objects.equals(this.username, rdBMSConnectionReq.username) &&
-            Objects.equals(this.password, rdBMSConnectionReq.password);
+                Objects.equals(this.connectionURL, rdBMSConnectionReq.connectionURL) &&
+                Objects.equals(this.username, rdBMSConnectionReq.username) &&
+                Objects.equals(this.connectionPassword, rdBMSConnectionReq.connectionPassword);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(driverName, connectionURL, username, password);
+        return Objects.hash(driverName, connectionURL, username, connectionPassword);
     }
 
     @Override
@@ -148,19 +150,19 @@ public class RDBMSConnectionReq  {
 
         StringBuilder sb = new StringBuilder();
         sb.append("class RDBMSConnectionReq {\n");
-        
+
         sb.append("    driverName: ").append(toIndentedString(driverName)).append("\n");
         sb.append("    connectionURL: ").append(toIndentedString(connectionURL)).append("\n");
         sb.append("    username: ").append(toIndentedString(username)).append("\n");
-        sb.append("    password: ").append(toIndentedString(password)).append("\n");
+        sb.append("    connectionPassword: ").append(toIndentedString(connectionPassword)).append("\n");
         sb.append("}");
         return sb.toString();
     }
 
     /**
-    * Convert the given object to string with each line indented by 4 spaces
-    * (except the first line).
-    */
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
     private String toIndentedString(java.lang.Object o) {
 
         if (o == null) {
