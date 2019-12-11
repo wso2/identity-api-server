40c0fd8a8626a55db96e505ded34d74e6c6f403d
Farasath Ahamed <farasatha@wso2.com>
2019-12-11 17:11:06 +0530

    Remove application name validation at swagger level
    Since application name regex is configurable, having a validation with a fixed regex could cause
    unexpected behaviours

--- components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/application/management/v1/ApplicationResponseModel.java
+++ components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/application/management/v1/ApplicationResponseModel.java
@@ -79,7 +79,7 @@ public class ApplicationResponseModel  {
     @JsonProperty("name")
     @Valid
     @NotNull(message = "Property name cannot be null.")
- @Pattern(regexp="^[a-zA-Z0-9._-]+(?: [a-zA-Z0-9._-]+)*$")
+
     public String getName() {
         return name;
     }
