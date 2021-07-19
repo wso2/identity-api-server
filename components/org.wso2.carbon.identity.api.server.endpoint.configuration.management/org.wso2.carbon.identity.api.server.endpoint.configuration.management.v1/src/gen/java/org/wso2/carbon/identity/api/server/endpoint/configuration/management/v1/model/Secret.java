af731e7a1c36feecfbd99e97dad22d75df8df85c
ChanikaRuchini <chanikaruchini.16@cse.mrt.ac.lk>
2021-07-19 13:34:51 +0530

    remove dispacher module

---
+++ components/org.wso2.carbon.identity.api.server.endpoint.configuration.management/org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/endpoint/configuration/management/v1/model/Secret.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.*;
+
+import java.util.Objects;
+import javax.validation.Valid;
+
+public class Secret {
+
+    private String key;
+    private String value;
+
+    /**
+     *
+     **/
+    public Secret key(String key) {
+
+        this.key = key;
+        return this;
+    }
+
+    @ApiModelProperty(example = "api-key", required = true, value = "")
+    @JsonProperty("key")
+    @Valid
+    @NotNull(message = "Property key cannot be null.")
+
+    public String getKey() {
+
+        return key;
+    }
+
+    public void setKey(String key) {
+
+        this.key = key;
+    }
+
+    /**
+     *
+     **/
+    public Secret value(String value) {
+
+        this.value = value;
+        return this;
+    }
+
+    @ApiModelProperty(example = "hsjsdjsadsahsahd", required = true, value = "")
+    @JsonProperty("value")
+    @Valid
+    @NotNull(message = "Property value cannot be null.")
+
+    public String getValue() {
+
+        return value;
+    }
+
+    public void setValue(String value) {
+
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Secret secret = (Secret) o;
+        return Objects.equals(this.key, secret.key) &&
+                Objects.equals(this.value, secret.value);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("class Secret {\n");
+
+        sb.append("    key: ").append(toIndentedString(key)).append("\n");
+        sb.append("    value: ").append(toIndentedString(value)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n");
+    }
+}
+
---
+++ components/org.wso2.carbon.identity.api.server.endpoint.configuration.management/org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1/src/main/java/org/wso2/carbon/identity/api/server/endpoint/configuration/management/v1/impl/EndpointConfigurationsApiServiceImpl.java
@@ -0,0 +1,82 @@
+/*
+* Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.wso2.carbon.identity.api.server.endpoint.configuration.management.common.ContextLoader;
+import org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1.EndpointConfigurationsApiService;
+import org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1.core.ConfigurationManagementService;
+import org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1.model.EndpointConfiguration;
+import org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1.model.EndpointConfigurationAdd;
+import org.wso2.carbon.identity.api.server.endpoint.configuration.management.v1.model.EndpointConfigurationUpdateRequest;
+
+import java.net.URI;
+import javax.ws.rs.core.Response;
+
+import static org.wso2.carbon.identity.api.server.endpoint.configuration.management.common.EndpointConfigurationManagementConstants.CONFIGURATION_CONTEXT_PATH;
+import static org.wso2.carbon.identity.api.server.endpoint.configuration.management.common.EndpointConfigurationManagementConstants.V1_API_PATH_COMPONENT;
+
+
+/**
+ * Implementation of endpoint Configuration Management REST API.
+ */
+public class EndpointConfigurationsApiServiceImpl implements EndpointConfigurationsApiService {
+
+    @Autowired
+    private ConfigurationManagementService configurationManagementService;
+
+    @Override
+    public Response createEndpointConfiguration(EndpointConfigurationAdd endpointConfigurationAdd) {
+
+        EndpointConfiguration endpointConfiguration = configurationManagementService.
+                addEndpointConfiguration(endpointConfigurationAdd);
+        URI location = ContextLoader.buildURIForHeader(V1_API_PATH_COMPONENT + CONFIGURATION_CONTEXT_PATH + "/"
+                + endpointConfiguration.getReferenceName());
+        return Response.created(location).entity(endpointConfiguration).build();
+    }
+
+    @Override
+    public Response deleteEndPointConfiguration(String referenceName) {
+
+        configurationManagementService.deleteEndpointConfiguration(referenceName);
+        return Response.noContent().build();
+    }
+
+    @Override
+    public Response getEndpointConfiguration(String referenceName) {
+
+        return Response.ok().entity(configurationManagementService.getEndpointConfiguration(referenceName))
+                .build();
+    }
+
+    @Override
+    public Response getEndpointConfigurationList() {
+
+        return Response.ok().entity(configurationManagementService.getEndpointConfigurations()).build();
+
+    }
+
+    @Override
+    public Response updateEndpointConfiguration(String referenceName, EndpointConfigurationUpdateRequest
+            endpointConfigurationUpdateRequest) {
+
+        return Response.ok()
+                .entity(configurationManagementService.updateEndpointConfiguration
+                        (referenceName, endpointConfigurationUpdateRequest)).build();
+    }
+}
