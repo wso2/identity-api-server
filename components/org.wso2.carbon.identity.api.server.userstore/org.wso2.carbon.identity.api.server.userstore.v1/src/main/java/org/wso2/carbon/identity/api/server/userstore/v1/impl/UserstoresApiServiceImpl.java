f5ea11e14fef1323d232a58286f4ebb697559da1
Ammoniya <ravindud@scorelab.org>
2021-08-19 14:35:18 +0530

    Add review comment

--- components/org.wso2.carbon.identity.api.server.userstore/org.wso2.carbon.identity.api.server.userstore.v1/src/main/java/org/wso2/carbon/identity/api/server/userstore/v1/impl/UserstoresApiServiceImpl.java
+++ components/org.wso2.carbon.identity.api.server.userstore/org.wso2.carbon.identity.api.server.userstore.v1/src/main/java/org/wso2/carbon/identity/api/server/userstore/v1/impl/UserstoresApiServiceImpl.java
@@ -44,6 +44,7 @@ public class UserstoresApiServiceImpl implements UserstoresApiService {
 
 
     private static boolean isAvailableUserStoreTypes(List<AvailableUserStoreClassesRes> userStoreList, String typeID) {
+
         for (AvailableUserStoreClassesRes userStore : userStoreList) {
             if (userStore.getTypeId().equals(typeID)) {
                 return true;
@@ -54,8 +55,9 @@ public class UserstoresApiServiceImpl implements UserstoresApiService {
 
     @Override
     public Response addUserStore(UserStoreReq userStoreReq) {
+
         if (!isAvailableUserStoreTypes(serverUserStoreService.getAvailableUserStoreTypes(), userStoreReq.getTypeId())) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("UserStore type is not allowed").build();
         }
         UserStoreResponse response = serverUserStoreService.addUserStore(userStoreReq);
         return Response.created(getResourceLocation(response.getId())).entity(response).build();
