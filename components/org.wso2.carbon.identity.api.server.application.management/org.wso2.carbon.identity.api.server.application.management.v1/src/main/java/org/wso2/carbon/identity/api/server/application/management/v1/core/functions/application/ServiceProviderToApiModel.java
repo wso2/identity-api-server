681c9028d3422b97227c48b22978cb121e77f471
Vivek Vinushanth Christopher <cvivekvinushanth@gmail.com>
2022-06-30 20:36:24 +0530

    fix with un-implemented

--- components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/main/java/org/wso2/carbon/identity/api/server/application/management/v1/core/functions/application/ServiceProviderToApiModel.java
+++ components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/main/java/org/wso2/carbon/identity/api/server/application/management/v1/core/functions/application/ServiceProviderToApiModel.java
@@ -64,6 +64,12 @@ import java.util.stream.Collectors;
 import static org.wso2.carbon.identity.api.server.application.management.v1.core.functions.Utils.arrayToStream;
 import static org.wso2.carbon.identity.api.server.application.management.v1.core.functions.application.UpdateAdvancedConfigurations.TYPE_JWKS;
 import static org.wso2.carbon.identity.api.server.application.management.v1.core.functions.application.UpdateAdvancedConfigurations.TYPE_PEM;
+import static org.wso2.carbon.identity.application.common.util.IdentityApplicationConstants.IS_MANAGEMENT_APP_SP_PROPERTY_NAME;
+import static org.wso2.carbon.identity.application.common.util.IdentityApplicationConstants.TEMPLATE_ID_SP_PROPERTY_NAME;
+import static org.wso2.carbon.identity.application.common.util.IdentityApplicationConstants.USE_USER_ID_FOR_DEFAULT_SUBJECT;
+import static org.wso2.carbon.identity.application.mgt.dao.impl.ApplicationDAOImpl.USE_DOMAIN_IN_ROLES;
+import static org.wso2.carbon.identity.base.IdentityConstants.SKIP_CONSENT;
+import static org.wso2.carbon.identity.base.IdentityConstants.SKIP_LOGOUT_CONSENT;
 
 /**
  * Converts the backend model ServiceProvider into the corresponding API model object.
@@ -351,7 +357,6 @@ public class ServiceProviderToApiModel implements Function<ServiceProvider, Appl
                 .returnAuthenticatedIdpList(authConfig.isAlwaysSendBackAuthenticatedListOfIdPs())
                 .skipLoginConsent(authConfig.isSkipConsent())
                 .skipLogoutConsent(authConfig.isSkipLogoutConsent())
-                .fragment(isFragmentApp(serviceProvider))
                 .additionalSpProperties(getSpProperties(serviceProvider))
                 .certificate(getCertificate(serviceProvider));
     }
@@ -359,21 +364,35 @@ public class ServiceProviderToApiModel implements Function<ServiceProvider, Appl
     private List<AdditionalSpProperty> getSpProperties(ServiceProvider serviceProvider) {
 
         ServiceProviderProperty[] serviceProviderProperties = serviceProvider.getSpProperties();
+        ServiceProviderProperty[] updatedSpProperties = removeAndSetSpProperties(serviceProviderProperties);
         List<AdditionalSpProperty> additionalSpProperties = new ArrayList<>();
-        if (serviceProviderProperties != null) {
-            for (ServiceProviderProperty serviceProviderProperty: serviceProviderProperties) {
-                AdditionalSpProperty spProperties = new AdditionalSpProperty();
-                if (StringUtils.isNotBlank(serviceProviderProperty.getName())) {
-                    spProperties.setName(serviceProviderProperty.getName());
-                    spProperties.setValue(serviceProviderProperty.getValue());
-                }
-                if (StringUtils.isNotBlank(serviceProviderProperty.getDisplayName())) {
-                    spProperties.setDisplayName(serviceProviderProperty.getDisplayName());
-                }
-                additionalSpProperties.add(spProperties);
+        for (ServiceProviderProperty serviceProviderProperty : updatedSpProperties) {
+            AdditionalSpProperty spProperties = new AdditionalSpProperty();
+            if (StringUtils.isNotBlank(serviceProviderProperty.getName())) {
+                spProperties.setName(serviceProviderProperty.getName());
+                spProperties.setValue(serviceProviderProperty.getValue());
             }
+            if (StringUtils.isNotBlank(serviceProviderProperty.getDisplayName())) {
+                spProperties.setDisplayName(serviceProviderProperty.getDisplayName());
+            }
+            additionalSpProperties.add(spProperties);
         }
         return additionalSpProperties;
+    }
+
+    private ServiceProviderProperty[] removeAndSetSpProperties(ServiceProviderProperty[] propertyList) {
+
+        /* These properties are part of advanced configurations and hence removing
+        them as they can't be packed as a part of additional sp properties again.*/
+            List<ServiceProviderProperty> spPropertyList =
+                    Arrays.stream(propertyList).collect(Collectors.toList());
+            spPropertyList.removeIf(property -> SKIP_CONSENT.equals(property.getName()));
+            spPropertyList.removeIf(property -> SKIP_LOGOUT_CONSENT.equals(property.getName()));
+            spPropertyList.removeIf(property -> USE_DOMAIN_IN_ROLES.equals(property.getName()));
+            spPropertyList.removeIf(property -> USE_USER_ID_FOR_DEFAULT_SUBJECT.equals(property.getName()));
+            spPropertyList.removeIf(property -> TEMPLATE_ID_SP_PROPERTY_NAME.equals(property.getName()));
+            spPropertyList.removeIf(property -> IS_MANAGEMENT_APP_SP_PROPERTY_NAME.equals(property.getName()));
+            return spPropertyList.toArray(new ServiceProviderProperty[0]);
     }
 
     private Certificate getCertificate(ServiceProvider serviceProvider) {
@@ -385,14 +404,6 @@ public class ServiceProviderToApiModel implements Function<ServiceProvider, Appl
         }
 
         return null;
-    }
-
-    private boolean isFragmentApp(ServiceProvider serviceProvider) {
-
-        return serviceProvider != null && serviceProvider.getSpProperties() != null &&
-                Arrays.stream(serviceProvider.getSpProperties())
-                        .filter(p -> IS_FRAGMENT_APP.equals(p.getName())).findFirst().map(
-                                p -> Boolean.valueOf(p.getValue())).orElse(Boolean.FALSE);
     }
 
     private Claim buildClaimModel(String claimUri) {
