ec1067ee80694f40f562d14f1aca2778bca0bba7
Ravindu Ramesh Perera <ravinduramesh@gmail.com>
2022-08-16 11:42:00 +0530

    fix findbug issue.

--- components/org.wso2.carbon.identity.api.server.challenge/org.wso2.carbon.identity.rest.api.server.challenge.v1/src/gen/java/org/wso2/carbon/identity/rest/api/server/challenge/v1/ApiException.java
+++ components/org.wso2.carbon.identity.api.server.challenge/org.wso2.carbon.identity.rest.api.server.challenge.v1/src/gen/java/org/wso2/carbon/identity/rest/api/server/challenge/v1/ApiException.java
@@ -1,8 +1,7 @@
 package org.wso2.carbon.identity.rest.api.server.challenge.v1;
 //comment
 public class ApiException extends Exception{
-	private int code;
-	public ApiException (int code, String msg) {
+	public ApiException (String msg) {
 		super(msg);
 	}
 }
