f5772cabdbd15bbf3676af5421592faac834eef4
kaya <kayathirikaya33@gmail.com>
2023-04-19 16:52:14 +0530

    Fix comments

--- components/org.wso2.carbon.identity.api.server.authenticators/org.wso2.carbon.identity.api.server.authenticators.common/src/main/java/org/wso2/carbon/identity/api/server/authenticators/common/Constants.java
+++ components/org.wso2.carbon.identity.api.server.authenticators/org.wso2.carbon.identity.api.server.authenticators.common/src/main/java/org/wso2/carbon/identity/api/server/authenticators/common/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
 
     public static final String AUTHENTICATOR_ERROR_PREFIX = "AUT-";
     public static final String FEDERATED_AUTHENTICATORS = "federatedAuthenticators";
-    public static final String AUTHENTICATOR_PATH_COMPONENT = "/identity-providers";
+    public static final String AUTHENTICATOR_PATH_COMPONENT = "/authenticators";
     public static final String PATH_SEPERATOR = "/";
     public static final String PAGE_LINK_REL_NEXT = "next";
     public static final String PAGE_LINK_REL_PREVIOUS = "previous";
