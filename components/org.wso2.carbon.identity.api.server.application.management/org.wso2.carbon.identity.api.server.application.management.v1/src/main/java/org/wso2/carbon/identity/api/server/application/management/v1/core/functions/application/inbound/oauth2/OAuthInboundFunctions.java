013e86dbfb1d6b029f8ebe3ab3c3287af811ca69
Gershom Seneviratne <gershom.seneviratne@gmail.com>
2023-07-11 10:02:17 +0530

    Updated APIs to remove functionality to support Legacy Audience behaviour

--- components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/main/java/org/wso2/carbon/identity/api/server/application/management/v1/core/functions/application/inbound/oauth2/OAuthInboundFunctions.java
+++ components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/main/java/org/wso2/carbon/identity/api/server/application/management/v1/core/functions/application/inbound/oauth2/OAuthInboundFunctions.java
@@ -36,7 +36,6 @@ import org.wso2.carbon.identity.oauth.IdentityOAuthAdminException;
 import org.wso2.carbon.identity.oauth.IdentityOAuthClientException;
 import org.wso2.carbon.identity.oauth.common.OAuthConstants;
 import org.wso2.carbon.identity.oauth.dto.OAuthConsumerAppDTO;
-import org.wso2.carbon.identity.oauth2.internal.OAuth2ServiceComponentHolder;
 
 import java.util.List;
 import java.util.UUID;
@@ -88,13 +87,6 @@ public class OAuthInboundFunctions {
 
                 if (!StringUtils.equals(oauthApp.getOauthConsumerSecret(), oidcConfigModel.getClientSecret())) {
                     throw buildBadRequestError("Invalid ClientSecret provided for update.");
-                }
-
-                if (OAuth2ServiceComponentHolder.isLegacyAudienceEnabled()) {
-                    if (oidcConfigModel.getAccessToken().getAudience() != null) {
-                        throw buildBadRequestError("Cannot set audiences for access token if legacy audiences " +
-                                "are enabled.");
-                    }
                 }
 
                 OAuthConsumerAppDTO appToUpdate = new ApiModelToOAuthConsumerApp().apply(application
