52fc742ec399ad5b290754c294fb264483139b8f
SujanSanjula96 <sanjulasujan@gmail.com>
2023-10-18 21:34:36 +0530

    Add support for API Resource properties

--- components/org.wso2.carbon.identity.api.server.api.resource/org.wso2.carbon.identity.api.server.api.resource.v1/src/main/java/org/wso2/carbon/identity/api/server/api/resource/v1/constants/APIResourceMgtEndpointConstants.java
+++ components/org.wso2.carbon.identity.api.server.api.resource/org.wso2.carbon.identity.api.server.api.resource.v1/src/main/java/org/wso2/carbon/identity/api/server/api/resource/v1/constants/APIResourceMgtEndpointConstants.java
@@ -33,8 +33,14 @@ public class APIResourceMgtEndpointConstants {
     public static final String API_RESOURCE_TYPE = "BUSINESS";
     public static final String API_RESOURCE_MANAGEMENT_PREFIX = "API-RESOURCE-";
     public static final String API_RESOURCE_PATH_COMPONENT = "/api-resources";
+
     private static final List<String> allowedAttributeList = new ArrayList<>();
     public static final List<String> ALLOWED_SEARCH_ATTRIBUTES = Collections.unmodifiableList(allowedAttributeList);
+
+    private static final List<String> supportedRequiredAttributeList = new ArrayList<>();
+    public static final List<String> SUPPORTED_REQUIRED_ATTRIBUTES = Collections
+            .unmodifiableList(supportedRequiredAttributeList);
+
     public static final String RESTRICTED_OAUTH2_SCOPES = "OAuth.RestrictedScopes.RestrictedScope";
     public static final Integer DEFAULT_LIMIT = 10;
     public static final String ASC_SORT_ORDER = "ASC";
@@ -45,6 +51,8 @@ public class APIResourceMgtEndpointConstants {
         allowedAttributeList.add("type");
         allowedAttributeList.add("requires_authorization");
         allowedAttributeList.add("scopes");
+
+        supportedRequiredAttributeList.add("properties");
     }
 
     /**
@@ -79,6 +87,8 @@ public class APIResourceMgtEndpointConstants {
                 "Limit should be a positive integer."),
         ERROR_CODE_BOTH_BEFORE_AFTER_PROVIDED("60011", "Invalid before/after provided.",
                 "Both before and after parameters cannot be provided at the same time."),
+        ERROR_CODE_INVALID_REQ_ATTRIBUTES("60012", "Invalid attribute name.",
+                "Invalid attribute name provided as required attribute."),
 
         // Server errors.
         ERROR_CODE_ADD_API_RESOURCE("65001", "Error while adding api resource.", "Server encountered an error while " +
