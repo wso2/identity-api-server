3c8260cff10693da2f9f014530fed17859fbe2ae
UdeshAthukorala <udeshathukorala@gmail.com>
2023-11-29 22:44:13 +0530

    Allow to update console's callback urls & allowed origins

--- components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/main/java/org/wso2/carbon/identity/api/server/application/management/v1/core/ServerApplicationManagementService.java
+++ components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/main/java/org/wso2/carbon/identity/api/server/application/management/v1/core/ServerApplicationManagementService.java
@@ -1036,7 +1036,18 @@ public class ServerApplicationManagementService {
          * But for now we are handling the validation here.
          */
         validateCORSOrigins(oidcConfigModel.getAllowedOrigins());
-        putInbound(applicationId, oidcConfigModel, OAuthInboundFunctions::putOAuthInbound);
+        boolean isAllowUpdateSystemApps =
+                isAllowUpdateSystemApplicationInboundOAuthConfigs(applicationId, oidcConfigModel);
+        try {
+            if (isAllowUpdateSystemApps) {
+                IdentityApplicationManagementUtil.setAllowUpdateSystemApplicationThreadLocal(true);
+            }
+            putInbound(applicationId, oidcConfigModel, OAuthInboundFunctions::putOAuthInbound);
+        } finally {
+            if (isAllowUpdateSystemApps) {
+                IdentityApplicationManagementUtil.removeAllowUpdateSystemApplicationThreadLocal();
+            }
+        }
     }
 
     public void putInboundSAMLConfiguration(String applicationId, SAML2Configuration saml2Configuration) {
@@ -1135,6 +1146,37 @@ public class ServerApplicationManagementService {
                     && applicationPatchModel.getClaimConfiguration() == null
                     && applicationPatchModel.getAdvancedConfigurations() == null
                     && applicationPatchModel.getProvisioningConfigurations() == null;
+        }
+        return false;
+    }
+
+    /**
+     * Check whether the updating system application's Inbound OAuth Configs are allowed or not.
+     *
+     * @param appId Application id.
+     * @param newOidcConfigModel New oidc config model.
+     * @return true if allowed.
+     */
+    private boolean isAllowUpdateSystemApplicationInboundOAuthConfigs(String appId,
+                                                                      OpenIDConnectConfiguration newOidcConfigModel) {
+
+        ServiceProvider application = getServiceProvider(appId);
+        Set<String> systemApplications =
+                ApplicationManagementServiceHolder.getApplicationManagementService().getSystemApplications();
+        if (systemApplications == null ||
+                systemApplications.stream().noneMatch(application.getApplicationName()::equalsIgnoreCase)) {
+            return false;
+        }
+
+        // For the Console it is allowed to update callbackURLs and allowedOrigins.
+        if (StringUtils.equalsIgnoreCase(CONSOLE_APP, application.getApplicationName())) {
+            OpenIDConnectConfiguration oldOidcConfigModel =
+                    getInbound(appId, OAUTH2, OAuthInboundFunctions::getOAuthConfiguration);
+            oldOidcConfigModel.setCallbackURLs(newOidcConfigModel.getCallbackURLs());
+            oldOidcConfigModel.setAllowedOrigins(newOidcConfigModel.getAllowedOrigins());
+            if (newOidcConfigModel.equals(oldOidcConfigModel)) {
+                return true;
+            }
         }
         return false;
     }
