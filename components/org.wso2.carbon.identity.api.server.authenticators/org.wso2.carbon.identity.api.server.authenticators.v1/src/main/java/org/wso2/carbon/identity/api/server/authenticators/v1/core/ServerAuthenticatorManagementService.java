81ac3b11c36988c3725f160eeeb61c371cb59da4
Hasanthi <hasanthidisanayakka@yahoo.com>
2023-12-01 17:18:42 +0530

    Revert "Set Api Based Authentication Flag"

--- components/org.wso2.carbon.identity.api.server.authenticators/org.wso2.carbon.identity.api.server.authenticators.v1/src/main/java/org/wso2/carbon/identity/api/server/authenticators/v1/core/ServerAuthenticatorManagementService.java
+++ components/org.wso2.carbon.identity.api.server.authenticators/org.wso2.carbon.identity.api.server.authenticators.v1/src/main/java/org/wso2/carbon/identity/api/server/authenticators/v1/core/ServerAuthenticatorManagementService.java
@@ -73,7 +73,6 @@ import static org.wso2.carbon.identity.api.server.common.Util.base64URLEncode;
 public class ServerAuthenticatorManagementService {
 
     private static final Log log = LogFactory.getLog(ServerAuthenticatorManagementService.class);
-    private static final String IS_API_BASED_SUPPORTED = "IS_API_BASED_SUPPORTED";
 
     /**
      * Retrieves the list of available authenticators.
@@ -512,11 +511,6 @@ public class ServerAuthenticatorManagementService {
         authenticator.setName(config.getName());
         authenticator.setDisplayName(config.getDisplayName());
         authenticator.setIsEnabled(config.isEnabled());
-        if (config.getProperties() != null && config.getProperties().length > 0 && IS_API_BASED_SUPPORTED.
-                equals(config.getProperties()[0].getName()))  {
-            authenticator.setIsAPIBasedAuthenticationSupported
-                    (Boolean.parseBoolean(config.getProperties()[0].getValue()));
-        }
         authenticator.setType(Authenticator.TypeEnum.LOCAL);
         String[] tags = config.getTags();
         if (ArrayUtils.isNotEmpty(tags)) {
