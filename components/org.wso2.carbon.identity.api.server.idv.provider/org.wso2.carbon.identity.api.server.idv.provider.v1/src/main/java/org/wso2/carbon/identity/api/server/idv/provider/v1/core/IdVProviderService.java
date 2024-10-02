c81c60200ec527ac4feed5a908ee960f58e400de
Afra Hussaindeen <mhfafra@gmail.com>
2024-10-02 18:43:04 +0530

    Support saving idvp logo url per idvp instance.

--- components/org.wso2.carbon.identity.api.server.idv.provider/org.wso2.carbon.identity.api.server.idv.provider.v1/src/main/java/org/wso2/carbon/identity/api/server/idv/provider/v1/core/IdVProviderService.java
+++ components/org.wso2.carbon.identity.api.server.idv.provider/org.wso2.carbon.identity.api.server.idv.provider.v1/src/main/java/org/wso2/carbon/identity/api/server/idv/provider/v1/core/IdVProviderService.java
@@ -222,6 +222,7 @@ public class IdVProviderService {
         idvProviderResponse.setName(idVProvider.getIdVProviderName());
         idvProviderResponse.setIsEnabled(idVProvider.isEnabled());
         idvProviderResponse.setDescription(idVProvider.getIdVProviderDescription());
+        idvProviderResponse.setImage(idVProvider.getImageUrl());
 
         if (idVProvider.getIdVConfigProperties() != null) {
             List<ConfigProperty> configProperties =
@@ -243,6 +244,7 @@ public class IdVProviderService {
         idVProvider.setIdVProviderName(idVProviderRequest.getName());
         idVProvider.setIdVProviderDescription(idVProviderRequest.getDescription());
         idVProvider.setEnabled(idVProviderRequest.getIsEnabled());
+        idVProvider.setImageUrl(idVProviderRequest.getImage());
         if (idVProviderRequest.getClaims() != null) {
             idVProvider.setClaimMappings(getClaimMap(idVProviderRequest.getClaims()));
         }
@@ -263,6 +265,7 @@ public class IdVProviderService {
         idVProvider.setIdVProviderName(idVProviderRequest.getName());
         idVProvider.setIdVProviderDescription(idVProviderRequest.getDescription());
         idVProvider.setEnabled(idVProviderRequest.getIsEnabled());
+        idVProvider.setImageUrl(idVProviderRequest.getImage());
         if (idVProviderRequest.getClaims() != null) {
             idVProvider.setClaimMappings(getClaimMap(idVProviderRequest.getClaims()));
         }
