94e7c77b21dcb02095db06ed43ab1b635b99dfee
sahandilshan <sahandilshan222@gmail.com>
2024-10-03 18:08:37 +0530

    Add api support to try out loginglow AI feature

--- components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.common/src/main/java/org/wso2/carbon/identity/api/server/application/management/common/ApplicationManagementServiceHolder.java
+++ components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.common/src/main/java/org/wso2/carbon/identity/api/server/application/management/common/ApplicationManagementServiceHolder.java
@@ -18,6 +18,7 @@ package org.wso2.carbon.identity.api.server.application.management.common;
 import org.wso2.carbon.identity.api.resource.mgt.APIResourceManager;
 import org.wso2.carbon.identity.application.mgt.ApplicationManagementService;
 import org.wso2.carbon.identity.application.mgt.AuthorizedAPIManagementService;
+import org.wso2.carbon.identity.application.mgt.ai.LoginFlowAIManager;
 import org.wso2.carbon.identity.cors.mgt.core.CORSManagementService;
 import org.wso2.carbon.identity.oauth.OAuthAdminServiceImpl;
 import org.wso2.carbon.identity.oauth.config.OAuthServerConfiguration;
@@ -43,6 +44,7 @@ public class ApplicationManagementServiceHolder {
     private static APIResourceManager apiResourceManager;
     private static AuthorizedAPIManagementService authorizedAPIManagementService;
     private static OrgApplicationManager orgApplicationManager;
+    private static LoginFlowAIManager loginFlowAIManagementService;
 
     public static ApplicationManagementService getApplicationManagementService() {
 
@@ -193,5 +195,23 @@ public class ApplicationManagementServiceHolder {
     public static void setOrgApplicationManager(OrgApplicationManager orgApplicationManager) {
 
         ApplicationManagementServiceHolder.orgApplicationManager = orgApplicationManager;
+    }
+
+    /**
+     * Get LoginFlowAIManagementService.
+     * @return LoginFlowAIManagementService
+     */
+    public static LoginFlowAIManager getLoginFlowAIManagementService() {
+
+        return loginFlowAIManagementService;
+    }
+
+    /**
+     * Set LoginFlowAIManagementService.
+     * @param loginFlowAIManagementService LoginFlowAIManager.
+     */
+    public static void setLoginFlowAIManagementService(LoginFlowAIManager loginFlowAIManagementService) {
+
+        ApplicationManagementServiceHolder.loginFlowAIManagementService = loginFlowAIManagementService;
     }
 }
