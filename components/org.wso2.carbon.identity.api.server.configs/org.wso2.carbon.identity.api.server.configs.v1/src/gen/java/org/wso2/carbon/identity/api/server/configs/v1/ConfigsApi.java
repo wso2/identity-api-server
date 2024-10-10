3b37e18462e9b6bc9f47e9a9c48e5712dc8f0d33
NipuniBhagya <nipunib@wso2.com>
2024-10-10 12:52:56 +0530

    Remove Spring dependencies in the configs module

--- components/org.wso2.carbon.identity.api.server.configs/org.wso2.carbon.identity.api.server.configs.v1/src/gen/java/org/wso2/carbon/identity/api/server/configs/v1/ConfigsApi.java
+++ components/org.wso2.carbon.identity.api.server.configs/org.wso2.carbon.identity.api.server.configs.v1/src/gen/java/org/wso2/carbon/identity/api/server/configs/v1/ConfigsApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2023-2024, WSO2 LLC. (http://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -18,12 +18,12 @@
 
 package org.wso2.carbon.identity.api.server.configs.v1;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 import java.io.InputStream;
 import java.util.List;
 
+import org.wso2.carbon.identity.api.server.configs.v1.factories.ConfigsApiServiceFactory;
 import org.wso2.carbon.identity.api.server.configs.v1.model.Authenticator;
 import org.wso2.carbon.identity.api.server.configs.v1.model.AuthenticatorListItem;
 import org.wso2.carbon.identity.api.server.configs.v1.model.CORSConfig;
@@ -59,8 +59,11 @@ import javax.validation.constraints.*;
 
 public class ConfigsApi  {
 
-    @Autowired
-    private ConfigsApiService delegate;
+    private final ConfigsApiService delegate;
+
+    public ConfigsApi() {
+        this.delegate = ConfigsApiServiceFactory.getConfigsApi();
+    }
 
     @Valid
     @GET
---
+++ components/org.wso2.carbon.identity.api.server.configs/org.wso2.carbon.identity.api.server.configs.v1/src/main/java/org/wso2/carbon/identity/api/server/configs/v1/factories/ServerConfigManagementServiceFactory.java
@@ -0,0 +1,77 @@
+package org.wso2.carbon.identity.api.server.configs.v1.factories;
+
+import org.wso2.carbon.identity.api.server.configs.common.ConfigsServiceHolder;
+import org.wso2.carbon.identity.api.server.configs.v1.core.ServerConfigManagementService;
+import org.wso2.carbon.identity.application.mgt.ApplicationManagementService;
+import org.wso2.carbon.identity.cors.mgt.core.CORSManagementService;
+import org.wso2.carbon.identity.oauth.dcr.DCRConfigurationMgtService;
+import org.wso2.carbon.identity.oauth2.impersonation.services.ImpersonationConfigMgtService;
+import org.wso2.carbon.identity.oauth2.token.handler.clientauth.jwt.core.JWTClientAuthenticatorMgtService;
+import org.wso2.carbon.idp.mgt.IdentityProviderManager;
+import org.wso2.carbon.logging.service.RemoteLoggingConfigService;
+
+/**
+ * Factory class for ServerConfigManagementService.
+ */
+public class ServerConfigManagementServiceFactory {
+
+    private static final ServerConfigManagementService SERVICE;
+
+    static {
+        ApplicationManagementService applicationManagementService = ConfigsServiceHolder
+                .getApplicationManagementService();
+        IdentityProviderManager identityProviderManager = ConfigsServiceHolder.getIdentityProviderManager();
+        CORSManagementService corsManagementService = ConfigsServiceHolder.getCorsManagementService();
+        RemoteLoggingConfigService remoteLoggingConfigService = ConfigsServiceHolder.getRemoteLoggingConfigService();
+        ImpersonationConfigMgtService impersonationConfigMgtService = ConfigsServiceHolder
+                .getImpersonationConfigMgtService();
+        JWTClientAuthenticatorMgtService jwtClientAuthenticatorMgtService = ConfigsServiceHolder
+                .getJWTClientAuthenticatorMgtService();
+        DCRConfigurationMgtService dcrConfigurationMgtService = ConfigsServiceHolder.getDcrConfigurationMgtService();
+
+        if (applicationManagementService == null) {
+            throw new IllegalStateException("ApplicationManagementService is not available from OSGi context.");
+        }
+
+        if (identityProviderManager == null) {
+            throw new IllegalStateException("IdentityProviderManager is not available from OSGi context.");
+        }
+
+        if (corsManagementService == null) {
+            throw new IllegalStateException("CORSManagementService is not available from OSGi context.");
+        }
+
+        if (remoteLoggingConfigService == null) {
+            throw new IllegalStateException("RemoteLoggingConfigService is not available from OSGi context.");
+        }
+
+        if (impersonationConfigMgtService == null) {
+            throw new IllegalStateException("ImpersonationConfigMgtService is not available from OSGi context.");
+        }
+
+        if (jwtClientAuthenticatorMgtService == null) {
+            throw new IllegalStateException("JWTClientAuthenticatorMgtService is not available from OSGi context.");
+        }
+
+        if (dcrConfigurationMgtService == null) {
+            throw new IllegalStateException("DCRConfigurationMgtService is not available from OSGi context.");
+        }
+
+        SERVICE = new ServerConfigManagementService(applicationManagementService, identityProviderManager,
+                corsManagementService,
+                remoteLoggingConfigService,
+                impersonationConfigMgtService,
+                dcrConfigurationMgtService,
+                jwtClientAuthenticatorMgtService);
+    }
+
+    /**
+     * Get ServerConfigManagementService instance.
+     *
+     * @return ServerConfigManagementService.
+     */
+    public static ServerConfigManagementService getServerConfigManagementService() {
+
+        return SERVICE;
+    }
+}
