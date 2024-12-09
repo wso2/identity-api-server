eff8fce2f0788befaf1fa271a29953a992b523bb
Nipuni Paaris <nipunib@wso2.com>
2024-12-09 16:16:50 +0530

    [Spring Cleanup] Remove Spring dependencies in authenticator management API (#688)
    * Remove Spring dependencies in authenticator management API
    
    * Resolve PR comments
    
    * Refactor ServerAuthenticatorManagementServiceFactory class

--- components/org.wso2.carbon.identity.api.server.authenticators/org.wso2.carbon.identity.api.server.authenticators.v1/src/main/java/org/wso2/carbon/identity/api/server/authenticators/v1/impl/AuthenticatorsApiServiceImpl.java
+++ components/org.wso2.carbon.identity.api.server.authenticators/org.wso2.carbon.identity.api.server.authenticators.v1/src/main/java/org/wso2/carbon/identity/api/server/authenticators/v1/impl/AuthenticatorsApiServiceImpl.java
@@ -1,24 +1,26 @@
 /*
-* Copyright (c) 2021, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2021-2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.wso2.carbon.identity.api.server.authenticators.v1.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.wso2.carbon.identity.api.server.authenticators.v1.AuthenticatorsApiService;
 import org.wso2.carbon.identity.api.server.authenticators.v1.core.ServerAuthenticatorManagementService;
+import org.wso2.carbon.identity.api.server.authenticators.v1.factories.ServerAuthenticatorManagementServiceFactory;
 
 import javax.ws.rs.core.Response;
 
@@ -27,8 +29,16 @@ import javax.ws.rs.core.Response;
  */
 public class AuthenticatorsApiServiceImpl implements AuthenticatorsApiService {
 
-    @Autowired
-    private ServerAuthenticatorManagementService authenticatorManagementService;
+    private final ServerAuthenticatorManagementService authenticatorManagementService;
+
+    public AuthenticatorsApiServiceImpl() {
+        try {
+            authenticatorManagementService = ServerAuthenticatorManagementServiceFactory
+                    .getServerAuthenticatorManagementService();
+        } catch (IllegalStateException e) {
+            throw new RuntimeException("Error occurred while initiating the authenticator management services.", e);
+        }
+    }
 
     @Override
     public Response authenticatorsGet(String filter, Integer limit, Integer offset) {
