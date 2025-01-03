26dff4f603fd570b75d043ac7266dc82e0b81ee3
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2025-01-03 14:33:44 +0530

    Generate DTOs.

--- components/org.wso2.carbon.identity.api.server.claim.management/org.wso2.carbon.identity.rest.api.server.claim.management.v1/src/gen/java/org/wso2/carbon/identity/rest/api/server/claim/management/v1/dto/LocalClaimResDTO.java
+++ components/org.wso2.carbon.identity.api.server.claim.management/org.wso2.carbon.identity.rest.api.server.claim.management.v1/src/gen/java/org/wso2/carbon/identity/rest/api/server/claim/management/v1/dto/LocalClaimResDTO.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021-2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.identity.rest.api.server.claim.management.v1.dto;
@@ -21,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.wso2.carbon.identity.rest.api.server.claim.management.v1.dto.AttributeMappingDTO;
 import org.wso2.carbon.identity.rest.api.server.claim.management.v1.dto.ClaimResDTO;
+import org.wso2.carbon.identity.rest.api.server.claim.management.v1.dto.ProfilesDTO;
 import org.wso2.carbon.identity.rest.api.server.claim.management.v1.dto.PropertyDTO;
 import io.swagger.annotations.*;
 import com.fasterxml.jackson.annotation.*;
@@ -76,6 +79,9 @@ public class LocalClaimResDTO extends ClaimResDTO {
 
     @Valid 
     private List<PropertyDTO> properties = new ArrayList<PropertyDTO>();
+
+    @Valid 
+    private ProfilesDTO profiles = null;
 
     /**
     * claim ID.
@@ -233,6 +239,18 @@ public class LocalClaimResDTO extends ClaimResDTO {
         this.properties = properties;
     }
 
+    /**
+     * Define attribute profiles.
+    **/
+    @ApiModelProperty(value = "Define any attribute profiles.")
+    @JsonProperty("profiles")
+    public ProfilesDTO getProfiles() {
+        return profiles;
+    }
+    public void setProfiles(ProfilesDTO profiles) {
+        this.profiles = profiles;
+    }
+
     @Override
     public String toString() {
 
@@ -253,6 +271,7 @@ public class LocalClaimResDTO extends ClaimResDTO {
         sb.append("    uniquenessScope: ").append(uniquenessScope).append("\n");
         sb.append("    attributeMapping: ").append(attributeMapping).append("\n");
         sb.append("    properties: ").append(properties).append("\n");
+        sb.append("    profiles: ").append(profiles).append("\n");
         
         sb.append("}\n");
         return sb.toString();
