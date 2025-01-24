3b4ef476d4f34a8eb3829cc1121ab9fcc5732aaa
lashini <lashini@wso2.com>
2025-01-24 17:27:55 +0530

    fix license headers and formattings

--- components/org.wso2.carbon.identity.api.server.idv.provider/org.wso2.carbon.identity.api.server.idv.provider.v1/src/gen/java/org/wso2/carbon/identity/api/server/idv/provider/v1/factories/IdvProvidersApiServiceFactory.java
+++ components/org.wso2.carbon.identity.api.server.idv.provider/org.wso2.carbon.identity.api.server.idv.provider.v1/src/gen/java/org/wso2/carbon/identity/api/server/idv/provider/v1/factories/IdvProvidersApiServiceFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -31,10 +31,10 @@ public class IdvProvidersApiServiceFactory {
    /**
     * Get IdvProvidersApiService.
     *
-    * @return IdvProvidersApiService
+    * @return IdvProvidersApiService.
     */
-   public static IdvProvidersApiService getIdvProvidersApi()
-   {
+   public static IdvProvidersApiService getIdvProvidersApi() {
+
       return SERVICE;
    }
 }
--- components/org.wso2.carbon.identity.api.server.idv.provider/org.wso2.carbon.identity.api.server.idv.provider.v1/src/main/java/org/wso2/carbon/identity/api/server/idv/provider/v1/factories/IdVProviderServiceFactory.java
+++ components/org.wso2.carbon.identity.api.server.idv.provider/org.wso2.carbon.identity.api.server.idv.provider.v1/src/main/java/org/wso2/carbon/identity/api/server/idv/provider/v1/factories/IdVProviderServiceFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -45,6 +45,7 @@ public class IdVProviderServiceFactory {
      * @return IdVProviderService
      */
     public static IdVProviderService getIdVProviderService() {
+
         return SERVICE;
     }
 }
