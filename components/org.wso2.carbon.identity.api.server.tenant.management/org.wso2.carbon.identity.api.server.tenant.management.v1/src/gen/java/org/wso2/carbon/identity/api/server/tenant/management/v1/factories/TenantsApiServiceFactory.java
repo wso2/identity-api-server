10380ec3067eccc113866ddec18761fc233d5f2b
Lashini Jayasekara <30428591+lashinijay@users.noreply.github.com>
2025-01-25 10:16:05 +0530

    Update components/org.wso2.carbon.identity.api.server.tenant.management/org.wso2.carbon.identity.api.server.tenant.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/tenant/management/v1/factories/TenantsApiServiceFactory.java
    Co-authored-by: Thamindu R <thamindu.randil@gmail.com>

--- components/org.wso2.carbon.identity.api.server.tenant.management/org.wso2.carbon.identity.api.server.tenant.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/tenant/management/v1/factories/TenantsApiServiceFactory.java
+++ components/org.wso2.carbon.identity.api.server.tenant.management/org.wso2.carbon.identity.api.server.tenant.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/tenant/management/v1/factories/TenantsApiServiceFactory.java
@@ -31,7 +31,7 @@ public class TenantsApiServiceFactory {
    /**
     * Get TenantsApiService.
     *
-    * @return TenantsApiService
+    * @return TenantsApiService.
     */
    public static TenantsApiService getTenantsApi()
    {
