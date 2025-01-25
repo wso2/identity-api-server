b7de3f460cd46ab9ca762238d6d76d157427a5be
Lashini Jayasekara <30428591+lashinijay@users.noreply.github.com>
2025-01-25 14:26:48 +0530

    [Spring Cleanup] Remove Spring dependencies in the script library (#812)

--- components/org.wso2.carbon.identity.api.server.script.library/org.wso2.carbon.identity.api.server.script.library.v1/src/main/java/org/wso2/carbon/identity/api/server/script/library/v1/impl/ScriptLibrariesApiServiceImpl.java
+++ components/org.wso2.carbon.identity.api.server.script.library/org.wso2.carbon.identity.api.server.script.library.v1/src/main/java/org/wso2/carbon/identity/api/server/script/library/v1/impl/ScriptLibrariesApiServiceImpl.java
@@ -1,29 +1,31 @@
 /*
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020-2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.identity.api.server.script.library.v1.impl;
 
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.wso2.carbon.identity.api.server.common.ContextLoader;
 import org.wso2.carbon.identity.api.server.common.error.APIError;
 import org.wso2.carbon.identity.api.server.common.error.ErrorResponse;
 import org.wso2.carbon.identity.api.server.script.library.common.Constants;
 import org.wso2.carbon.identity.api.server.script.library.v1.ScriptLibrariesApiService;
 import org.wso2.carbon.identity.api.server.script.library.v1.core.ServerScriptLibrariesService;
+import org.wso2.carbon.identity.api.server.script.library.v1.factories.ServerScriptLibrariesServiceFactory;
 import org.wso2.carbon.identity.api.server.script.library.v1.model.ScriptLibraryListResponse;
 import org.wso2.carbon.identity.api.server.script.library.v1.model.ScriptLibraryResponse;
 
@@ -44,8 +46,16 @@ import static org.wso2.carbon.identity.api.server.script.library.common.Constant
  */
 public class ScriptLibrariesApiServiceImpl implements ScriptLibrariesApiService {
 
-    @Autowired
-    private ServerScriptLibrariesService serverScriptLibrariesService;
+    private final ServerScriptLibrariesService serverScriptLibrariesService;
+
+    public ScriptLibrariesApiServiceImpl() {
+
+        try {
+            this.serverScriptLibrariesService = ServerScriptLibrariesServiceFactory.getServerScriptLibrariesService();
+        } catch (IllegalStateException e) {
+            throw new RuntimeException("Error occurred while initiating ServerScriptLibrariesService.", e);
+        }
+    }
 
     @Override
     public Response addScriptLibrary(String name, InputStream contentInputStream, Attachment contentDetail,
