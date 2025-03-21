067bac5e4a4800d301f86b233108914a93e7f484
roshanemoraes <moraes.git@gmail.com>
2025-03-21 07:11:32 +0530

    Add AsyncOperationStatusMgt.

---
+++ components/org.wso2.carbon.identity.api.server.asynchronous.operation.status.management/org.wso2.carbon.identity.api.server.asynchronous.operation.status.management.common/src/main/java/org/wso2/carbon/identity/api/server/asynchronous/operation/status/management/common/AsyncOperationStatusMgtServiceHolder.java
@@ -0,0 +1,31 @@
+package org.wso2.carbon.identity.api.server.asynchronous.operation.status.management.common;
+
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.identity.framework.async.status.mgt.AsyncStatusMgtService;
+
+/**
+ * Holds the services which the async operation status management API component is using.
+ */
+public class AsyncOperationStatusMgtServiceHolder {
+
+    private AsyncOperationStatusMgtServiceHolder() {
+
+    }
+
+    private static class AsyncStatusMgtServiceHolder {
+
+        private static final AsyncStatusMgtService SERVICE =
+                (AsyncStatusMgtService) PrivilegedCarbonContext.getThreadLocalCarbonContext()
+                        .getOSGiService(AsyncStatusMgtService.class, null);
+    }
+
+    /**
+     * Get AsyncStatusMgtService service.
+     *
+     * @return AsyncStatusMgtService.
+     */
+    public static AsyncStatusMgtService getAsyncStatusMgtService() {
+
+        return AsyncOperationStatusMgtServiceHolder.AsyncStatusMgtServiceHolder.SERVICE;
+    }
+}
---
+++ components/org.wso2.carbon.identity.api.server.asynchronous.operation.status.management/org.wso2.carbon.identity.api.server.asynchronous.operation.status.management.common/src/main/java/org/wso2/carbon/identity/api/server/asynchronous/operation/status/management/common/constants/AsyncOperationStatusMgt.java
@@ -0,0 +1,5 @@
+package org.wso2.carbon.identity.api.server.asynchronous.operation.status.management.common.constants;
+
+public class AsyncOperationStatusMgt {
+
+}
