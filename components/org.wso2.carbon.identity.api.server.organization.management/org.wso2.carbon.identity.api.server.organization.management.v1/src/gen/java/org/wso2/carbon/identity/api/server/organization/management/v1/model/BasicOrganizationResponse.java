a7fac13fedb6213bc02cf0a47ae962f2dfb04b28
Hasini Samarathunga <hasinis@wso2.com>
2025-03-26 10:39:24 +0530

    Introduce Organization Identifier to Organization Management API

--- components/org.wso2.carbon.identity.api.server.organization.management/org.wso2.carbon.identity.api.server.organization.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/organization/management/v1/model/BasicOrganizationResponse.java
+++ components/org.wso2.carbon.identity.api.server.organization.management/org.wso2.carbon.identity.api.server.organization.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/organization/management/v1/model/BasicOrganizationResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -37,6 +37,7 @@ public class BasicOrganizationResponse  {
   
     private String id;
     private String name;
+    private String organizationHandle;
 
 @XmlType(name="StatusEnum")
 @XmlEnum(String.class)
@@ -113,6 +114,26 @@ public enum StatusEnum {
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+    **/
+    public BasicOrganizationResponse organizationHandle(String organizationHandle) {
+
+        this.organizationHandle = organizationHandle;
+        return this;
+    }
+    
+    @ApiModelProperty(example = "abcbuilders", required = true, value = "")
+    @JsonProperty("organizationHandle")
+    @Valid
+    @NotNull(message = "Property organizationHandle cannot be null.")
+
+    public String getOrganizationHandle() {
+        return organizationHandle;
+    }
+    public void setOrganizationHandle(String organizationHandle) {
+        this.organizationHandle = organizationHandle;
     }
 
     /**
@@ -195,6 +216,7 @@ public enum StatusEnum {
         BasicOrganizationResponse basicOrganizationResponse = (BasicOrganizationResponse) o;
         return Objects.equals(this.id, basicOrganizationResponse.id) &&
             Objects.equals(this.name, basicOrganizationResponse.name) &&
+            Objects.equals(this.organizationHandle, basicOrganizationResponse.organizationHandle) &&
             Objects.equals(this.status, basicOrganizationResponse.status) &&
             Objects.equals(this.ref, basicOrganizationResponse.ref) &&
             Objects.equals(this.attributes, basicOrganizationResponse.attributes);
@@ -202,7 +224,7 @@ public enum StatusEnum {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, status, ref, attributes);
+        return Objects.hash(id, name, organizationHandle, status, ref, attributes);
     }
 
     @Override
@@ -213,6 +235,7 @@ public enum StatusEnum {
         
         sb.append("    id: ").append(toIndentedString(id)).append("\n");
         sb.append("    name: ").append(toIndentedString(name)).append("\n");
+        sb.append("    organizationHandle: ").append(toIndentedString(organizationHandle)).append("\n");
         sb.append("    status: ").append(toIndentedString(status)).append("\n");
         sb.append("    ref: ").append(toIndentedString(ref)).append("\n");
         sb.append("    attributes: ").append(toIndentedString(attributes)).append("\n");
