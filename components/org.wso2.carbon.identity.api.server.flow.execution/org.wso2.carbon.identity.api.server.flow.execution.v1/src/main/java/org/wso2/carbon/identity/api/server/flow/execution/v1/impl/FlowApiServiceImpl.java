f527a5a73619516401404d673b06d18cd522240b
nilasini <nilasini@wso2.com>
2025-06-09 13:56:42 +0530

    Remove unnecessary changes

--- components/org.wso2.carbon.identity.api.server.flow.execution/org.wso2.carbon.identity.api.server.flow.execution.v1/src/main/java/org/wso2/carbon/identity/api/server/flow/execution/v1/impl/FlowApiServiceImpl.java
+++ components/org.wso2.carbon.identity.api.server.flow.execution/org.wso2.carbon.identity.api.server.flow.execution.v1/src/main/java/org/wso2/carbon/identity/api/server/flow/execution/v1/impl/FlowApiServiceImpl.java
@@ -18,18 +18,31 @@
 
 package org.wso2.carbon.identity.api.server.flow.execution.v1.impl;
 
-import org.wso2.carbon.identity.api.server.flow.execution.v1.*;
-import org.wso2.carbon.identity.api.server.flow.execution.v1.*;
-import java.util.List;
+import org.wso2.carbon.identity.api.server.flow.execution.v1.FlowApiService;
+import org.wso2.carbon.identity.api.server.flow.execution.v1.FlowExecutionRequest;
+import org.wso2.carbon.identity.api.server.flow.execution.v1.core.FlowExecutionServiceCore;
+import org.wso2.carbon.identity.api.server.flow.execution.v1.factories.FlowExecutionServiceFactory;
 
 import javax.ws.rs.core.Response;
 
+/**
+ * Implementation of the FlowApiService interface.
+ */
 public class FlowApiServiceImpl implements FlowApiService {
+
+    private final FlowExecutionServiceCore flowExecutionServiceCore;
+
+    public FlowApiServiceImpl() {
+
+        this.flowExecutionServiceCore = FlowExecutionServiceFactory.getFlowExecutionService();
+    }
 
     @Override
     public Response flowExecutePost(FlowExecutionRequest flowExecutionRequest) {
 
-        // do some magic!
-        return Response.ok().entity("magic!").build();
+        return Response.ok()
+                .entity(flowExecutionServiceCore.processFlowExecution((flowExecutionRequest)))
+                .build();
     }
 }
+
