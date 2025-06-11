28c7099235f9fe94eba26b20b4c0c241ed150bb8
sahandilshan <sahandilshan222@gmail.com>
2025-06-11 10:26:03 +0530

    Add missing stubs

--- components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/application/management/v1/SharedOrganizationsResponse.java
+++ components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/application/management/v1/SharedOrganizationsResponse.java
@@ -25,6 +25,7 @@ import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
 import org.wso2.carbon.identity.api.server.application.management.v1.BasicOrganizationResponse;
+import org.wso2.carbon.identity.api.server.application.management.v1.Link;
 import javax.validation.constraints.*;
 
 
@@ -35,10 +36,38 @@ import javax.xml.bind.annotation.*;
 
 public class SharedOrganizationsResponse  {
   
+    private List<Link> links = null;
+
     private List<BasicOrganizationResponse> organizations = null;
 
 
     /**
+    **/
+    public SharedOrganizationsResponse links(List<Link> links) {
+
+        this.links = links;
+        return this;
+    }
+    
+    @ApiModelProperty(example = "[{\"href\":\"/api/server/v1/applications/67f5a202-48c4-4313-9327-016da5f08f17/share?limit=10&recursive=false&next=MTA=\",\"rel\":\"next\"},{\"href\":\"/api/server/v1/applications/67f5a202-48c4-4313-9327-016da5f08f17/share?limit=10&recursive=false&after=MTA=\",\"rel\":\"previous\"}]", value = "")
+    @JsonProperty("links")
+    @Valid
+    public List<Link> getLinks() {
+        return links;
+    }
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+
+    public SharedOrganizationsResponse addLinksItem(Link linksItem) {
+        if (this.links == null) {
+            this.links = new ArrayList<>();
+        }
+        this.links.add(linksItem);
+        return this;
+    }
+
+        /**
     **/
     public SharedOrganizationsResponse organizations(List<BasicOrganizationResponse> organizations) {
 
@@ -76,12 +105,13 @@ public class SharedOrganizationsResponse  {
             return false;
         }
         SharedOrganizationsResponse sharedOrganizationsResponse = (SharedOrganizationsResponse) o;
-        return Objects.equals(this.organizations, sharedOrganizationsResponse.organizations);
+        return Objects.equals(this.links, sharedOrganizationsResponse.links) &&
+            Objects.equals(this.organizations, sharedOrganizationsResponse.organizations);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(organizations);
+        return Objects.hash(links, organizations);
     }
 
     @Override
@@ -90,6 +120,7 @@ public class SharedOrganizationsResponse  {
         StringBuilder sb = new StringBuilder();
         sb.append("class SharedOrganizationsResponse {\n");
         
+        sb.append("    links: ").append(toIndentedString(links)).append("\n");
         sb.append("    organizations: ").append(toIndentedString(organizations)).append("\n");
         sb.append("}");
         return sb.toString();
