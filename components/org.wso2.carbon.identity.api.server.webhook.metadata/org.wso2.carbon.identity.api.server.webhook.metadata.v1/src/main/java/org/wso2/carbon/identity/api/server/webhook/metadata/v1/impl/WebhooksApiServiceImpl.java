d250378302ab4f6e377cb958bc0b6b0ab024e73e
Sachin-Mamoru <sachinm@wso2.com>
2025-07-27 22:25:05 +0530

    Onboarded webhook metadata properties api support

--- components/org.wso2.carbon.identity.api.server.webhook.metadata/org.wso2.carbon.identity.api.server.webhook.metadata.v1/src/main/java/org/wso2/carbon/identity/api/server/webhook/metadata/v1/impl/WebhooksApiServiceImpl.java
+++ components/org.wso2.carbon.identity.api.server.webhook.metadata/org.wso2.carbon.identity.api.server.webhook.metadata.v1/src/main/java/org/wso2/carbon/identity/api/server/webhook/metadata/v1/impl/WebhooksApiServiceImpl.java
@@ -23,6 +23,7 @@ import org.wso2.carbon.identity.api.server.webhook.metadata.v1.core.ServerWebhoo
 import org.wso2.carbon.identity.api.server.webhook.metadata.v1.factories.ServerWebhookMetadataServiceFactory;
 import org.wso2.carbon.identity.api.server.webhook.metadata.v1.model.EventProfile;
 import org.wso2.carbon.identity.api.server.webhook.metadata.v1.model.WebhookMetadata;
+import org.wso2.carbon.identity.api.server.webhook.metadata.v1.model.WebhookMetadataProperties;
 
 import javax.ws.rs.core.Response;
 
@@ -47,5 +48,14 @@ public class WebhooksApiServiceImpl implements WebhooksApiService {
                 ServerWebhookMetadataServiceFactory.getServerWebhookMetadataService();
         WebhookMetadata webhookMetadata = webhookMetadataService.getWebhookMetadata();
         return Response.ok().entity(webhookMetadata).build();
+    }
+
+    @Override
+    public Response patchWebhookMetadata(WebhookMetadataProperties webhookMetadataProperties) {
+
+        ServerWebhookMetadataService webhookMetadataService =
+                ServerWebhookMetadataServiceFactory.getServerWebhookMetadataService();
+        return Response.ok().entity(webhookMetadataService.updateWebhookMetadataProperties(webhookMetadataProperties))
+                .build();
     }
 }
