86921fcbea8d185f852732bc1122715ba96379e2
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2025-09-08 13:30:13 +0530

    Add SELF_REGISTER_USER operation type

--- components/org.wso2.carbon.identity.api.server.workflow/org.wso2.carbon.identity.rest.api.server.workflow.v1/src/gen/java/org/wso2/carbon/identity/rest/api/server/workflow/v1/model/Operation.java
+++ components/org.wso2.carbon.identity.api.server.workflow/org.wso2.carbon.identity.rest.api.server.workflow.v1/src/gen/java/org/wso2/carbon/identity/rest/api/server/workflow/v1/model/Operation.java
@@ -32,7 +32,7 @@ import javax.xml.bind.annotation.XmlEnumValue;
 @XmlEnum(String.class)
 public enum Operation {
 
-    @XmlEnumValue("ADD_USER") ADD_USER(String.valueOf("ADD_USER")), @XmlEnumValue("DELETE_USER") DELETE_USER(String.valueOf("DELETE_USER")), @XmlEnumValue("UPDATE_ROLES_OF_USERS") UPDATE_ROLES_OF_USERS(String.valueOf("UPDATE_ROLES_OF_USERS")), @XmlEnumValue("ADD_ROLE") ADD_ROLE(String.valueOf("ADD_ROLE")), @XmlEnumValue("DELETE_ROLE") DELETE_ROLE(String.valueOf("DELETE_ROLE")), @XmlEnumValue("UPDATE_ROLE_NAME") UPDATE_ROLE_NAME(String.valueOf("UPDATE_ROLE_NAME")), @XmlEnumValue("UPDATE_USERS_OF_ROLES") UPDATE_USERS_OF_ROLES(String.valueOf("UPDATE_USERS_OF_ROLES")), @XmlEnumValue("DELETE_USER_CLAIMS") DELETE_USER_CLAIMS(String.valueOf("DELETE_USER_CLAIMS")), @XmlEnumValue("UPDATE_USER_CLAIMS") UPDATE_USER_CLAIMS(String.valueOf("UPDATE_USER_CLAIMS"));
+    @XmlEnumValue("ADD_USER") ADD_USER(String.valueOf("ADD_USER")), @XmlEnumValue("SELF_REGISTER_USER") SELF_REGISTER_USER(String.valueOf("SELF_REGISTER_USER")), @XmlEnumValue("DELETE_USER") DELETE_USER(String.valueOf("DELETE_USER")), @XmlEnumValue("UPDATE_ROLES_OF_USERS") UPDATE_ROLES_OF_USERS(String.valueOf("UPDATE_ROLES_OF_USERS")), @XmlEnumValue("ADD_ROLE") ADD_ROLE(String.valueOf("ADD_ROLE")), @XmlEnumValue("DELETE_ROLE") DELETE_ROLE(String.valueOf("DELETE_ROLE")), @XmlEnumValue("UPDATE_ROLE_NAME") UPDATE_ROLE_NAME(String.valueOf("UPDATE_ROLE_NAME")), @XmlEnumValue("UPDATE_USERS_OF_ROLES") UPDATE_USERS_OF_ROLES(String.valueOf("UPDATE_USERS_OF_ROLES")), @XmlEnumValue("DELETE_USER_CLAIMS") DELETE_USER_CLAIMS(String.valueOf("DELETE_USER_CLAIMS")), @XmlEnumValue("UPDATE_USER_CLAIMS") UPDATE_USER_CLAIMS(String.valueOf("UPDATE_USER_CLAIMS"));
 
 
     private String value;
