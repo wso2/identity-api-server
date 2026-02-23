972a70741fbe0b0f313e3a2a226efac5e56f2155
Madhavi Gayathri <madhavi.17@cse.mrt.ac.lk>
2026-02-24 01:55:01 +0530

    Support multiple certificate in IDP export-import with json filetype.

---
+++ components/org.wso2.carbon.identity.api.server.idp/org.wso2.carbon.identity.api.server.idp.v1/src/main/java/org/wso2/carbon/identity/api/server/idp/v1/model/IdentityProviderExportResponse.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.api.server.idp.v1.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.application.common.model.IdentityProvider;
+
+import java.util.Objects;
+
+/**
+ * Identity Provider Export Response model.
+ */
+public class IdentityProviderExportResponse extends IdentityProvider {
+
+    private static final long serialVersionUID = 1L;
+    private static final Log log = LogFactory.getLog(IdentityProviderExportResponse.class);
+
+    private Certificate certificate;
+
+    @JsonProperty("certificate")
+    public Certificate getCertificates() {
+
+        if (log.isDebugEnabled()) {
+            log.debug("Retrieving certificate from IdentityProviderExportResponse");
+        }
+        return certificate;
+    }
+
+    @JsonProperty("certificate")
+    public void setCertificate(Certificate certificate) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("Setting certificate in IdentityProviderExportResponse");
+        }
+        this.certificate = certificate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        IdentityProviderExportResponse that = (IdentityProviderExportResponse) o;
+        return Objects.equals(certificate, that.certificate);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), certificate);
+    }
+}
