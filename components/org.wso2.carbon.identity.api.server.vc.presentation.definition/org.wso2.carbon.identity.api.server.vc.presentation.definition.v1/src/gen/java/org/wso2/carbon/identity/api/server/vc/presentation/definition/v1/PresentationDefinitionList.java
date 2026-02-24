ba1e36348039bf33e5fd262b911a2edcab6299c0
Udeepa_Gallage <117582139+udeepa15@users.noreply.github.com>
2026-02-24 18:57:20 +0530

    Add VP presentation definition API module
    Introduce a new Presentation Definition Management API module for Verifiable Presentations (VP). Adds two submodules: a common module (pom, service holder, constants) and a v1 API module (pom, JAX-RS resource, generated models, service interface/factory, core service implementation, API impl and OpenAPI YAML). The core service integrates with PresentationDefinitionService via PrivilegedCarbonContext, handles CRUD operations, model conversions, and maps VP exceptions to REST errors. Also updates the root pom.xml to include the new components.

---
+++ components/org.wso2.carbon.identity.api.server.vc.presentation.definition/org.wso2.carbon.identity.api.server.vc.presentation.definition.v1/src/gen/java/org/wso2/carbon/identity/api/server/vc/presentation/definition/v1/PresentationDefinitionList.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.api.server.vc.presentation.definition.v1;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * List response model for presentation definitions.
+ */
+@ApiModel(description = "List of presentation definitions")
+public class PresentationDefinitionList {
+
+    private Integer totalResults;
+    private List<PresentationDefinitionListItem> presentationDefinitions = new ArrayList<>();
+
+    @ApiModelProperty(value = "Total number of presentation definitions.")
+    @JsonProperty("totalResults")
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    @ApiModelProperty(value = "List of presentation definitions.")
+    @JsonProperty("presentationDefinitions")
+    public List<PresentationDefinitionListItem> getPresentationDefinitions() {
+        return presentationDefinitions;
+    }
+
+    public void setPresentationDefinitions(List<PresentationDefinitionListItem> presentationDefinitions) {
+        this.presentationDefinitions = presentationDefinitions;
+    }
+}
