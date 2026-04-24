4d78cb786643f91559f8bb41098712119c3e0b9b
vimukthiRajapaksha <reachvimukthi@gmail.com>
2026-04-24 17:28:39 +0530

    feat(fapi2): add tenant-level FAPI config models and connector utility
    Add FapiConfig and FapiProfile model classes to the configs API for tenant-level FAPI compliance configuration (enabled flag + supported profiles)
    
    Add FapiProfile enum to the application management API module
    
    Introduce FAPIConnectorUtil for bi-directional conversion between API-layer FAPI models and OAuth bundle FAPI models
    
    Default FAPI profile to FAPI1_ADVANCED when an application is FAPI-enabled but no profile is explicitly specified
    
    Remove unused imports (FapiConfig, FapiProfile, FapiConfigMgtServerException)
    
    Fix import ordering in ServerApplicationMetadataService

--- components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/main/java/org/wso2/carbon/identity/api/server/application/management/v1/core/ServerApplicationMetadataService.java
+++ components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/main/java/org/wso2/carbon/identity/api/server/application/management/v1/core/ServerApplicationMetadataService.java
@@ -18,6 +18,7 @@
 
 package org.wso2.carbon.identity.api.server.application.management.v1.core;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
---
+++ components/org.wso2.carbon.identity.api.server.configs/org.wso2.carbon.identity.api.server.configs.v1/src/gen/java/org/wso2/carbon/identity/api/server/configs/v1/model/FapiProfile.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.api.server.configs.v1.model;
+
+import io.swagger.annotations.ApiModel;
+import org.wso2.carbon.identity.oauth2.fapi.models.FapiProfileEnum;
+
+import javax.validation.constraints.*;
+
+/**
+ * FAPI security profile. - &#x60;FAPI1_ADVANCED&#x60;: FAPI 1.0 Advanced Security Profile. - &#x60;FAPI2_SECURITY&#x60;: FAPI 2.0 Security Profile. 
+ **/
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+
+@XmlType(name="")
+@XmlEnum(String.class)
+public enum FapiProfile {
+
+    @XmlEnumValue("FAPI1_ADVANCED") FAPI1_ADVANCED(String.valueOf("FAPI1_ADVANCED")), @XmlEnumValue("FAPI2_SECURITY") FAPI2_SECURITY(String.valueOf("FAPI2_SECURITY"));
+
+
+    private String value;
+
+    FapiProfile(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    public static FapiProfile fromValue(String value) {
+
+        for (FapiProfile b : FapiProfile.values()) {
+            if (b.value.equals(value)) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public static FapiProfile fromValue(FapiProfileEnum fapiProfileEnum) {
+
+        if (fapiProfileEnum == null) {
+            return null;
+        }
+        return fromValue(fapiProfileEnum.value());
+    }
+}
+
+
+
