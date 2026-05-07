2ab82b44351b28cf9b7d428292143f444aedf9c0
bhagyasakalanka <bsakalanka9@gmail.com>
2026-05-07 18:33:59 +0530

    Add APIs for moesif publisher

---
+++ components/org.wso2.carbon.identity.api.server.moesif.publisher/org.wso2.carbon.identity.api.server.moesif.publisher.v1/src/gen/java/org/wso2/carbon/identity/api/server/moesif/publisher/v1/MoesifPublishersApi.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.api.server.moesif.publisher.v1;
+
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.ext.multipart.Multipart;
+import java.io.InputStream;
+import java.util.List;
+
+import org.wso2.carbon.identity.api.server.moesif.publisher.v1.model.Error;
+import org.wso2.carbon.identity.api.server.moesif.publisher.v1.model.MoesifPublisher;
+import org.wso2.carbon.identity.api.server.moesif.publisher.v1.model.MoesifPublisherAdd;
+import org.wso2.carbon.identity.api.server.moesif.publisher.v1.model.MoesifPublisherUpdate;
+import org.wso2.carbon.identity.api.server.moesif.publisher.v1.MoesifPublishersApiService;
+import org.wso2.carbon.identity.api.server.moesif.publisher.v1.factories.MoesifPublishersApiServiceFactory;
+
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import io.swagger.annotations.*;
+
+import javax.validation.constraints.*;
+
+@Path("/moesif-publishers")
+@Api(description = "The moesif-publishers API")
+
+public class MoesifPublishersApi  {
+
+    private final MoesifPublishersApiService delegate;
+
+    public MoesifPublishersApi() {
+
+        this.delegate = MoesifPublishersApiServiceFactory.getMoesifPublishersApi();
+    }
+
+    @Valid
+    @POST
+    
+    @Consumes({ "application/json" })
+    @Produces({ "application/json" })
+    @ApiOperation(value = "Create a Moesif event publisher", notes = "", response = MoesifPublisher.class, tags={ "Moesif Publishers", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 201, message = "Successfully created", response = MoesifPublisher.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = Error.class),
+        @ApiResponse(code = 409, message = "Conflict", response = Error.class),
+        @ApiResponse(code = 500, message = "Internal Server Error", response = Error.class)
+    })
+    public Response createMoesifPublisher(@ApiParam(value = "" ,required=true) @Valid MoesifPublisherAdd moesifPublisherAdd) {
+
+        return delegate.createMoesifPublisher(moesifPublisherAdd );
+    }
+
+    @Valid
+    @DELETE
+    @Path("/{publisher-name}")
+    
+    @Produces({ "application/json" })
+    @ApiOperation(value = "Delete a Moesif event publisher", notes = "", response = Void.class, tags={ "Moesif Publishers", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 204, message = "Successfully deleted", response = Void.class),
+        @ApiResponse(code = 404, message = "Not Found", response = Error.class),
+        @ApiResponse(code = 500, message = "Internal Server Error", response = Error.class)
+    })
+    public Response deleteMoesifPublisher(@ApiParam(value = "Name of the Moesif publisher.",required=true) @PathParam("publisher-name") String publisherName) {
+
+        return delegate.deleteMoesifPublisher(publisherName );
+    }
+
+    @Valid
+    @GET
+    @Path("/{publisher-name}")
+    
+    @Produces({ "application/json" })
+    @ApiOperation(value = "Get a Moesif event publisher by name", notes = "", response = MoesifPublisher.class, tags={ "Moesif Publishers", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "Successful operation", response = MoesifPublisher.class),
+        @ApiResponse(code = 404, message = "Not Found", response = Error.class),
+        @ApiResponse(code = 500, message = "Internal Server Error", response = Error.class)
+    })
+    public Response getMoesifPublisher(@ApiParam(value = "Name of the Moesif publisher.",required=true) @PathParam("publisher-name") String publisherName) {
+
+        return delegate.getMoesifPublisher(publisherName );
+    }
+
+    @Valid
+    @GET
+    
+    
+    @Produces({ "application/json" })
+    @ApiOperation(value = "Get all Moesif event publishers", notes = "", response = MoesifPublisher.class, responseContainer = "List", tags={ "Moesif Publishers", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "Successful operation", response = MoesifPublisher.class, responseContainer = "List"),
+        @ApiResponse(code = 500, message = "Internal Server Error", response = Error.class)
+    })
+    public Response getMoesifPublishers() {
+
+        return delegate.getMoesifPublishers();
+    }
+
+    @Valid
+    @PATCH
+    @Path("/{publisher-name}")
+    @Consumes({ "application/json" })
+    @Produces({ "application/json" })
+    @ApiOperation(value = "Update the API key of a Moesif event publisher", notes = "", response = MoesifPublisher.class, tags={ "Moesif Publishers" })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "Successfully updated", response = MoesifPublisher.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = Error.class),
+        @ApiResponse(code = 404, message = "Not Found", response = Error.class),
+        @ApiResponse(code = 500, message = "Internal Server Error", response = Error.class)
+    })
+    public Response patchMoesifPublisher(@ApiParam(value = "Name of the Moesif publisher.",required=true) @PathParam("publisher-name") String publisherName, @ApiParam(value = "" ,required=true) @Valid MoesifPublisherUpdate moesifPublisherUpdate) {
+
+        return delegate.patchMoesifPublisher(publisherName,  moesifPublisherUpdate );
+    }
+
+}
