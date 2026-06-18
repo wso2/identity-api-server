a41ecaef9ada9de1aa064706a590b9db6be0f3b5
Dinitha Wijewardhana <dini2040gh@gmail.com>
2026-06-18 12:09:26 +0530

    Add REST API module for VP Presentation Definition management

---
+++ components/org.wso2.carbon.identity.api.server.vp.template.management/org.wso2.carbon.identity.api.server.vp.template.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/vp/template/management/v1/RequestedCredentialModel.java
@@ -0,0 +1,61 @@
+package org.wso2.carbon.identity.api.server.vp.template.management.v1;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * Requested Credential Model.
+ */
+@ApiModel(description = "Requested Credential Model")
+public class RequestedCredentialModel {
+
+    @NotNull
+    private String type;
+    private String purpose;
+    private String issuer;
+    private List<String> claims;
+
+    @ApiModelProperty(required = true, value = "Type of the requested credential.")
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @ApiModelProperty(value = "Purpose of requesting the credential.")
+    @JsonProperty("purpose")
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    @ApiModelProperty(value = "The trusted issuer for this credential.")
+    @JsonProperty("issuer")
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    @ApiModelProperty(value = "List of claims requested from this credential.")
+    @JsonProperty("claims")
+    public List<String> getClaims() {
+        return claims;
+    }
+
+    public void setClaims(List<String> claims) {
+        this.claims = claims;
+    }
+}
