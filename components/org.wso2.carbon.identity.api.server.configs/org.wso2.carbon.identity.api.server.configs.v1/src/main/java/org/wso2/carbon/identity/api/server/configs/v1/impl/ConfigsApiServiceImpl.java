7e811f82898a8053fd28b0f54e7a2391fa91aded
RovinKYK <kykrovin@gmail.com>
2026-06-25 17:26:02 +0530

    Add missing functions

--- components/org.wso2.carbon.identity.api.server.configs/org.wso2.carbon.identity.api.server.configs.v1/src/main/java/org/wso2/carbon/identity/api/server/configs/v1/impl/ConfigsApiServiceImpl.java
+++ components/org.wso2.carbon.identity.api.server.configs/org.wso2.carbon.identity.api.server.configs.v1/src/main/java/org/wso2/carbon/identity/api/server/configs/v1/impl/ConfigsApiServiceImpl.java
@@ -22,6 +22,7 @@ import org.wso2.carbon.identity.api.server.configs.v1.ConfigsApiService;
 import org.wso2.carbon.identity.api.server.configs.v1.core.ServerConfigManagementService;
 import org.wso2.carbon.identity.api.server.configs.v1.factories.ServerConfigManagementServiceFactory;
 import org.wso2.carbon.identity.api.server.configs.v1.model.AgentConfigPatch;
+import org.wso2.carbon.identity.api.server.configs.v1.model.ApplicationObject;
 import org.wso2.carbon.identity.api.server.configs.v1.model.CORSPatch;
 import org.wso2.carbon.identity.api.server.configs.v1.model.CompatibilitySettings;
 import org.wso2.carbon.identity.api.server.configs.v1.model.DCRPatch;
@@ -183,6 +184,13 @@ public class ConfigsApiServiceImpl implements ConfigsApiService {
     }
 
     @Override
+    public Response removeApplicationFromPurpose(String purposeId, String applicationId) {
+
+        configManagementService.removeApplicationFromPurpose(purposeId, applicationId);
+        return Response.noContent().build();
+    }
+
+    @Override
     public Response getSchemas() {
 
         return Response.ok().entity(configManagementService.getSchemas()).build();
@@ -339,6 +347,12 @@ public class ConfigsApiServiceImpl implements ConfigsApiService {
         return Response.noContent().build();
     }
 
+    @Override
+    public Response getApplicationsForPurpose(String purposeId) {
+
+        return Response.ok().entity(configManagementService.getApplicationsForPurpose(purposeId)).build();
+    }
+
     /**
      * Gets the OAuth2 inbound authentication configuration of an organization.
      *
@@ -361,6 +375,13 @@ public class ConfigsApiServiceImpl implements ConfigsApiService {
 
         configManagementService.updateOAuth2InboundAuthConfig(inboundAuthOAuth2Config);
         return Response.ok().build();
+    }
+
+    @Override
+    public Response addApplicationToPurpose(String purposeId, ApplicationObject applicationObject) {
+
+        configManagementService.addApplicationToPurpose(purposeId, applicationObject.getId());
+        return Response.status(Response.Status.CREATED).build();
     }
 
     /**
